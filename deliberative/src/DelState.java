a2c3a1f3216ca1c12bf956cd3795c70836dc1c0b
Boris Goullet <boris.goullet@gmail.com>
2017-10-19 16:01:34 +0200

    DelState Actions

--- deliberative/src/DelState.java
+++ deliberative/src/DelState.java
@@ -1,3 +1,4 @@
+import logist.plan.Action;
 import logist.task.Task;
 import logist.task.TaskSet;
 import logist.topology.Topology;
@@ -6,111 +7,121 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DelState {
-
+    
     TaskSet worldTasks;
     TaskSet pickedTasks;
     Topology.City location;
     double totalCost = 0;
-
+    ArrayList<Action> actions = new ArrayList<>();
+    
     int capacity;
-
+    
     public DelState(TaskSet t, TaskSet t_, Topology.City location, int capacity) {
         this.worldTasks = t;
         this.pickedTasks = t_;
         this.location = location;
         this.capacity = capacity;
     }
-
+    
     public List<DelState> nextStates(int costPerKm) {
-
+        
         ArrayList<DelState> l = new ArrayList<>();
-
+        
         if (worldTasks.isEmpty() && pickedTasks.isEmpty()) {
             return l;
         }
-
-
-        //Next deliveries states
+        
+        // Next deliveries states
         for (Task t : pickedTasks) {
             DelState tmp = new DelState(this);
             tmp.pickedTasks.remove(t);
-
+            
             tmp.addCost(costPerKm * location.distanceTo(t.deliveryCity));
-
+            
+            tmp.addAction(new Action.Delivery(t));
+            
             capacity += t.weight;
             
             l.add(tmp);
         }
-
-        //Next pickup tasks
+        
+        // Next pickup tasks
         for (Task t : worldTasks) {
-
+            
             DelState tmp = new DelState(this);
-
+            
             if (capacity >= t.weight) {
                 tmp.worldTasks.remove(t);
                 tmp.pickedTasks.add(t);
-
+                
                 tmp.addCost(costPerKm * location.distanceTo(t.pickupCity));
-
+                
+                tmp.addAction(new Action.Pickup(t));
+                
                 capacity -= t.weight;
-
+                
                 l.add(tmp);
             }
-
-
+            
         }
-
-
+        
         return l;
     }
-
+    
     public DelState(DelState d) {
         this(d.getWorldTasks().clone(), d.getPickedTasks().clone(), d.getLocation(), d.getCapacity());
         totalCost = d.getTotalCost();
+        actions = d.getActions();
     }
-
+    
     public double getTotalCost() {
         return totalCost;
     }
-
+    
     public TaskSet getWorldTasks() {
         return worldTasks;
     }
-
+    
     public void setWorldTasks(TaskSet worldTasks) {
         this.worldTasks = worldTasks;
     }
-
+    
     public TaskSet getPickedTasks() {
         return pickedTasks;
     }
-
+    
     public void setPickedTasks(TaskSet pickedTasks) {
         this.pickedTasks = pickedTasks;
     }
-
+    
     public Topology.City getLocation() {
         return location;
     }
-
+    
     public void setLocation(Topology.City location) {
         this.location = location;
     }
-
+    
     public int getCapacity() {
         return capacity;
     }
-
+    
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
     
     public void addCost(double d) {
-        this.totalCost += d; 
+        this.totalCost += d;
+    }
+    
+    public void addAction(Action a) {
+        actions.add(a);
+    }
+    
+    public ArrayList<Action> getActions() {
+        return new ArrayList<>(actions);
     }
 }
-
 
 class Tuple<X, Y> {
     public final X _1;
