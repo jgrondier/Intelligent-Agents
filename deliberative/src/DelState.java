9a0d18c9a66c6549ee99bdd3601c08cc5d69d438
Boris Goullet <boris.goullet@gmail.com>
2017-10-19 16:21:50 +0200

    fixed capacity and locations

--- deliberative/src/DelState.java
+++ deliberative/src/DelState.java
@@ -16,9 +16,9 @@ public class DelState {
     
     int capacity;
     
-    public DelState(TaskSet t, TaskSet t_, Topology.City location, int capacity) {
-        this.worldTasks = t;
-        this.pickedTasks = t_;
+    public DelState(TaskSet worldTasks, TaskSet carriedTasks, Topology.City location, int capacity) {
+        this.worldTasks = worldTasks;
+        this.pickedTasks = carriedTasks;
         this.location = location;
         this.capacity = capacity;
     }
@@ -35,12 +35,13 @@ public class DelState {
         for (Task t : pickedTasks) {
             DelState tmp = new DelState(this);
             tmp.pickedTasks.remove(t);
+            tmp.setLocation(t.deliveryCity);
             
             tmp.addCost(costPerKm * location.distanceTo(t.deliveryCity));
             
             tmp.addAction(new Action.Delivery(t));
             
-            capacity += t.weight;
+            tmp.setCapacity(capacity + t.weight);
             
             l.add(tmp);
         }
@@ -53,12 +54,13 @@ public class DelState {
             if (capacity >= t.weight) {
                 tmp.worldTasks.remove(t);
                 tmp.pickedTasks.add(t);
+                tmp.setLocation(t.pickupCity);
                 
                 tmp.addCost(costPerKm * location.distanceTo(t.pickupCity));
                 
                 tmp.addAction(new Action.Pickup(t));
-                
-                capacity -= t.weight;
+
+                tmp.setCapacity(capacity - t.weight);
                 
                 l.add(tmp);
             }
@@ -93,7 +95,7 @@ public class DelState {
     public void setPickedTasks(TaskSet pickedTasks) {
         this.pickedTasks = pickedTasks;
     }
-    
+
     public Topology.City getLocation() {
         return location;
     }
@@ -120,31 +122,5 @@ public class DelState {
     
     public ArrayList<Action> getActions() {
         return new ArrayList<>(actions);
-    }
-}
-
-class Tuple<X, Y> {
-    public final X _1;
-    public final Y _2;
-    
-    public Tuple(X _1, Y _2) {
-        this._1 = _1;
-        this._2 = _2;
-    }
-    
-    @Override
-    public String toString() {
-        return "(" + _1.toString() + ", " + _2.toString() + ")";
-    }
-    
-    @Override
-    public boolean equals(Object obj) {
-        if (getClass() != obj.getClass() || ((Tuple<?, ?>) obj)._1.getClass() != _1.getClass()
-                || ((Tuple<?, ?>) obj)._2.getClass() != _2.getClass()) {
-            return false;
-        }
-        @SuppressWarnings("unchecked") // is actually checked just above
-        Tuple<X, Y> o = (Tuple<X, Y>) obj;
-        return this._1.equals((o._1)) && this._2.equals((o._2));
     }
 }
